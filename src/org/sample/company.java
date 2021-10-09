ee1e159ac3b115b8544f9b4484a298ee3a997551
shakthiivignesh <shakthivignesh29@gmail.com>
2021-10-09 12:38:12 +0530

    msg

---
+++ src/org/sample/company.java
@@ -0,0 +1,27 @@
+package org.sample;
+
+public class company {
+public static void main(String[] args) {
+	byte age=25;
+	System.out.println("Emplayee age is:"+age);
+	short atmpin=4567;
+	System.out.println("employee atm pin is:"+atmpin);
+	long mobile=856484884844l;
+	System.out.println("employee mobile number is:"+mobile);
+	int landline=454545454;
+	System.out.println("employee landline number is:"+landline);
+	float salary=34567.32f;
+	System.out.println("employee salary is:"+salary);
+	boolean a=true;
+	System.out.println(a);
+	char grade='A';
+	System.out.println("employee grade is:"+grade);
+    String email="qwerty123@gmail.com";
+    System.out.println("employee email is:"+email);
+    float x=20/4f;
+    System.out.println(x);
+    double y=20/4d;
+    System.out.println(y);
+	
+}
+}
